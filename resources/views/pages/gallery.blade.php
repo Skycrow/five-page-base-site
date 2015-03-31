855b44b273a37264c899ac922d17ebbed55676d3
Shane <shane@shanelawes.com>
2015-03-31 13:57:39 +0200

    Added gallery page with modal

--- resources/views/pages/gallery.blade.php
+++ resources/views/pages/gallery.blade.php
@@ -4,10 +4,79 @@
 
 <h1>Gallery</h1>
 
-	<h3>Photos I Like:</h3>
+	    <!-- Page Content -->
+    <div class="container">
 
-	<p>
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas expedita animi maxime voluptatum pariatur, in voluptates qui, voluptate ea accusamus nemo quo perferendis quasi molestias cumque quia sed aut earum!
-	</p>
+        <div class="row">
+
+            <div class="col-lg-12">
+                <h1 class="page-header">Thumbnail Gallery</h1>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail " href="{{ asset('img/photo-1.jpg') }}" data-toggle="lightbox" data-gallery="multiimages" data-title="Photo one" >
+                    <img class="img-responsive" src="{{ asset('img/photo-1.jpg') }}" alt="">
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="{{ asset('img/photo-2.jpg') }}" data-toggle="lightbox" data-gallery="multiimages" data-title="Photo two">
+                    <img class="img-responsive" src="{{ asset('img/photo-2.jpg') }}" alt="">
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="{{ asset('img/photo-3.jpg') }}" data-toggle="lightbox" data-gallery="multiimages" data-title="Photo three">
+                    <img class="img-responsive" src="{{ asset('img/photo-3.jpg') }}" alt="">
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="{{ asset('img/photo-4.jpg') }}" data-toggle="lightbox" data-gallery="multiimages" data-title="Photo four">
+                    <img class="img-responsive" src="{{ asset('img/photo-4.jpg') }}" alt="">
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="{{ asset('img/photo-5.jpg') }}" data-toggle="lightbox" data-gallery="multiimages" data-title="Photo five">
+                    <img class="img-responsive" src="{{ asset('img/photo-5.jpg') }}" alt="">
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="{{ asset('img/photo-6.jpg') }}" data-toggle="lightbox" data-gallery="multiimages" data-title="Photo six">
+                    <img class="img-responsive" src="{{ asset('img/photo-6.jpg') }}" alt="">
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="{{ asset('img/photo-7.jpg') }}" data-toggle="lightbox" data-gallery="multiimages" data-title="Photo seven">
+                    <img class="img-responsive" src="{{ asset('img/photo-7.jpg') }}" alt="">
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="{{ asset('img/photo-8.jpg') }}" data-toggle="lightbox" data-gallery="multiimages" data-title="Photo eight">
+                    <img class="img-responsive" src="{{ asset('img/photo-8.jpg') }}" alt="">
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="{{ asset('img/photo-9.jpg') }}" data-toggle="lightbox" data-gallery="multiimages" data-title="Photo nine">
+                    <img class="img-responsive" src="{{ asset('img/photo-9.jpg') }}" alt="">
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="{{ asset('img/photo-10.jpg') }}" data-toggle="lightbox" data-gallery="multiimages" data-title="Photo ten">
+                    <img class="img-responsive" src="{{ asset('img/photo-10.jpg') }}" alt="">
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="{{ asset('img/photo-11.jpg') }}" data-toggle="lightbox" data-gallery="multiimages" data-title="Photo eleven">
+                    <img class="img-responsive" src="{{ asset('img/photo-11.jpg') }}" alt="">
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="{{ asset('img/photo-12.jpg') }}" data-toggle="lightbox" data-gallery="multiimages" data-title="Photo twelve">
+                    <img class="img-responsive" src="{{ asset('img/photo-12.jpg') }}" alt="">
+                </a>
+            </div>
+        </div>
+
+        <hr>
+
+    </div>
+    <!-- /.container -->
 
 @stop
